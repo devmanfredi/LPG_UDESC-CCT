cfbb7d2af3ae7ff44f358f98603e7874303065a7
Heuler Felipe Ramos Manfredi <45863981+devmanfredi@users.noreply.github.com>
2019-09-12 17:23:17 -0300

    Update 2.c
    correção

--- TRABALHO COMPLEMENTAR 1/2.c
+++ TRABALHO COMPLEMENTAR 1/2.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
 int fib(int n) {
-    if (n<=2) {
+    if (n == 0 || n == 1) {
         return 1;
     }
-    int a = fib(n-2);
-    int b = fib(n-1);
-    return a+b;
+    else{
+    	return fib(n - 1) + fib(n - 2);	
+	}
+    
 }
 
 int main()
 {
-    int n;
+    int n,i;
+    printf("\tSerie Fibonacci\t\n");
+    printf("\n");
+    printf("Digite o tamanho da sequencia: ");
     scanf("%d", &n);
-    printf("%d ", fib(n));
+    printf("\tResultado\t\n");
+    for (i = 0; i < n; i++){
+    	printf("%d ", fib(i));
+	}
+    
 
     return 0;
+}
