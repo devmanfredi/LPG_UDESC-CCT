b27d1b6bb2454b6825795d8b4d275658e684c471
Heuler Felipe Ramos Manfredi <45863981+devmanfredi@users.noreply.github.com>
2019-09-12 17:24:58 -0300

    Update lib.h

--- TRABALHO COMPLEMENTAR 1/lib.h
+++ TRABALHO COMPLEMENTAR 1/lib.h
@@ -15,10 +15,11 @@ float divi(float a, float b) {
 }
 
 int fib(int n) {
-    if (n<=2) {
+    if (n == 0 || n == 1) {
         return 1;
     }
-    int a = fib(n-2);
-    int b = fib(n-1);
-    return a+b;
+    else{
+    	return fib(n - 1) + fib(n - 2);	
+	}
+    
 }
